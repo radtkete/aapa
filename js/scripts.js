90f2888d4568a862379b6f891ac4d7656048460b
Taylor Radtke <tradtke@aapa.org>
2016-11-30 15:09:04 -0500

    added students page

--- js/scripts.js
+++ js/scripts.js
@@ -444,16 +444,29 @@ function init_expander(){
 function init_accordion(){
     (function($){
 
-
         $('.js-accordion-trigger').bind('click', function(e){
-          jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
-          jQuery(this).parent().toggleClass('is-expanded');
-          jQuery(this).find('.fa-plus').toggleClass("fa-minus");
-          e.preventDefault();
+
+        $btn  = $(this);
+        $icon = $btn.find("i");
+        $btn.parent().find('.accordion-body').slideToggle('fast');  // apply the toggle to the ul
+        $btn.parent().toggleClass('is-expanded');
+
+        if ($icon.hasClass("fa-plus")) {
+            $icon.removeClass("fa-plus");
+            $icon.addClass("fa-minus");
+        }
+        else if ($icon.hasClass("fa-minus")) {
+            $icon.removeClass("fa-minus");
+            $icon.addClass("fa-plus");
+        }
+
+
         });
 
     })(jQuery);
 }
+
+
 
 
 /* ---------------------------------------------
