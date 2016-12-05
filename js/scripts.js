289db4880bdd1ce52dce9d9a79eb08ca67251a86
Taylor Radtke <tradtke@aapa.org>
2016-12-05 17:05:10 -0500

    finished new mobile bav only one item

--- js/scripts.js
+++ js/scripts.js
@@ -571,3 +571,36 @@ $(function(){
         event.preventDefault();
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.nav-accordion-trigger').bind('click', function(e){
+  jQuery(this).parent().find('.submenu').slideToggle('fast'); 
+ // apply the toggle to the ul
+  jQuery(this).parent().toggleClass('is-expanded');
+  e.preventDefault();
+});
